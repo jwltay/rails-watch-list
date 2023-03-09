724645e7ca5e36ccd767917707b5537fb575fc34
Leanne Tay <duckingville@yahoo.com>
2023-03-09 14:46:04 +0800

    added lists controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,2 @@
 class MoviesController < ApplicationController
-  before_action :set_movie
-
-  def index
-    @movies = Movie.all
-  end
-
-  def show
-    @movie
-  end
-
-  private
-
-  def set_movie
-    @movie = Movie.find(params[:id])
-  end
 end
