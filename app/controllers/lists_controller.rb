069b451e6f4364417270774d96eda6126d626196
Leanne Tay <duckingville@yahoo.com>
2023-03-09 16:11:36 +0800

    users can add new list via new page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list
+  before_action :set_list, only: %i[show]
   # list index
   # show list
   # new list
@@ -10,11 +10,29 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
---
+++ db/migrate/20230309080529_add_unique_bookmark_constraints_to_bookmarks.rb
@@ -0,0 +1,5 @@
+class AddUniqueBookmarkConstraintsToBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    add_index :bookmarks, %i[movie_id list_id]
+  end
+end
