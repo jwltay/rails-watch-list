feb000c2cfe7ec944afbd5b07febf15abc375c1b
Leanne Tay <duckingville@yahoo.com>
2023-03-09 14:41:06 +0800

    added individual movie page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,17 @@
 class MoviesController < ApplicationController
-  # before_action :set_movie
+  before_action :set_movie
+
   def index
     @movies = Movie.all
+  end
+
+  def show
+    @movie
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
   end
 end
